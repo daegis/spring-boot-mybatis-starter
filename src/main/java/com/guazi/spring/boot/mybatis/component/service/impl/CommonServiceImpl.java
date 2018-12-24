8ebac792dece8e3b740018d5779dcc3c9b0045e6
xianyingda <xianyingda@shouqiev.com>
2018-12-24 11:26:03 +0800

    Initial commit

---
+++ src/main/java/com/guazi/spring/boot/mybatis/component/service/impl/CommonServiceImpl.java
@@ -0,0 +1,24 @@
+package com.guazi.spring.boot.mybatis.component.service.impl;
+
+
+import com.guazi.spring.boot.mybatis.component.spi.ICommonDao;
+
+/**
+ * @author XianYingda
+ */
+public class CommonServiceImpl extends DefaultServiceImpl {
+
+    private ICommonDao commonDao;
+
+    @Override
+    public ICommonDao getCommonDao() {
+        return commonDao;
+    }
+
+    @Override
+    public void setCommonDao(ICommonDao commonDao) {
+        this.commonDao = commonDao;
+    }
+
+
+}
---
+++ src/main/java/com/guazi/spring/boot/mybatis/component/spi/ICommonDao.java
@@ -0,0 +1,8 @@
+package com.guazi.spring.boot.mybatis.component.spi;
+
+/**
+ * @author XianYingda
+ */
+public interface ICommonDao extends IMapper {
+
+}
